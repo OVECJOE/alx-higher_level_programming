8adaa35e903062134f0007904140c21ee6dd8317
OVECJOE <ovecjoe123@gmail.com>
2021-09-08 13:55:23 +0100

    Added 8-uppercase.py

---
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def uppercase(str):
+    for c in str:
+        isupper = ord(c) >= 65 and ord(c) <= 90
+        isnumeric = ord(c) >= 48 and ord(c) <= 57
+        print("{:c}".format(ord(c) if isupper or isnumeric else ord(c) - 32),\
+                end="")
+    print();
