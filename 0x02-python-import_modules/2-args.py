26be27b98bda02a0d9385b09f781e12f713d01b0
OVECJOE <ovecjoe123@gmail.com>
2021-09-10 08:17:20 +0000

    Fixed bug in 2-args.py

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,9 +1,8 @@
 #!/usr/bin/python3
 from sys import argv
 
-if __name__ == "__main__":
-    argc = len(argv) - 1
-    print("{:d} arguments{}".format(argc, ":" if argc else "."))
-    if argc:
-        for i in range(1, argc + 1):
-            print("{:d}: {}".format(i, argv[i]))
+argc = len(argv) - 1
+print("{:d} arguments{}".format(argc, ":" if argc else "."))
+if argc:
+    for i in range(1, argc + 1):
+        print("{:d}: {}".format(i, argv[i]))
