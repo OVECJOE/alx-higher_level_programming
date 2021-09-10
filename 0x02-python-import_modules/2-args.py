14ae73077716687769e78a8e6a12eea257e82c9f
OVECJOE <ovecjoe123@gmail.com>
2021-09-10 08:03:57 +0000

    Added 2-args.py

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+from sys import argv
+
+if __name__ == "__main__":
+    argc = len(argv) - 1
+    print("{:d} arguments{}".format(argc, ":" if argc else "."))
+    if argc:
+        for i in range(1, argc + 1):
+            print("{:d}: {}".format(i, argv[i]))
