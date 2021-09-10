506eeb3dda4e8d7a1000b89fe6afd6e557f7ba9c
OVECJOE <ovecjoe123@gmail.com>
2021-09-10 17:15:51 +0000

    Added 100-my_calculator.py

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -1,23 +1,24 @@
 #!/usr/bin/python3
 from sys import argv
 import calculator_1 as calc
-argc = len(argv) - 1
-if argc != 3:
-    print("Usage: {} <a> <operator> <b>".format(argv[0]))
-    exit(1)
-a = int(argv[1])
-operator = argv[2]
-b = int(argv[3])
+if __name__ == "__main__":
+    argc = len(argv) - 1
+    if argc != 3:
+        print("Usage: {} <a> <operator> <b>".format(argv[0]))
+        exit(1)
+    a = int(argv[1])
+    operator = argv[2]
+    b = int(argv[3])
 
-if operator == "+":
-    result = calc.add(a, b)
-elif operator == "-":
-    result = calc.sub(a, b)
-elif operator == "*":
-    result = calc.mul(a, b)
-elif operator == "/":
-    result = calc.div(a, b)
-else:
-    print("Unknown operator. Available operators: +, -, * and /")
-    exit(1)
-print("{:d} {} {:d} = {:d}".format(a, operator, b, result))
+    if operator == "+":
+        result = calc.add(a, b)
+    elif operator == "-":
+        result = calc.sub(a, b)
+    elif operator == "*":
+        result = calc.mul(a, b)
+    elif operator == "/":
+        result = calc.div(a, b)
+    else:
+        print("Unknown operator. Available operators: +, -, * and /")
+        exit(1)
+    print("{:d} {} {:d} = {:d}".format(a, operator, b, result))
