0b452aa43c6231dfd58e5d849efa380a5c349e61
OVECJOE <ovecjoe123@gmail.com>
2021-10-03 00:48:57 +0000

    Added 101-square.py

--- 0x06-python-classes/101-square.py
+++ 0x06-python-classes/101-square.py
@@ -42,17 +42,15 @@ class Square:
         self.__position = value
 
     def __str__(self):
-        """Format for output"""
-        if self.__size == 0:
-            print()
+        """Define the print() representation of a Square."""
         if self.__size != 0:
-            [print("") for i in range(self.__position[1])]
-        for i in range(self.__size):
-            [print(" ", end="") for j in range(self.__position[0])]
-            [print("#", end="") for k in range(self.__size)]
+            [print("") for i in range(0, self.__position[1])]
+        for i in range(0, self.__size):
+            [print(" ", end="") for j in range(0, self.__position[0])]
+            [print("#", end="") for k in range(0, self.__size)]
             if i != self.__size - 1:
-                print()
-        return ""
+                print("")
+        return ("")
 
     def area(self):
         """Return the current area of the square."""
