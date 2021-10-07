02848a19aa4aba9e7122241a0b66b17f1980bd63
OVECJOE <ovecjoe123@gmail.com>
2021-10-07 21:12:33 +0000

    Added: 101-locked_class.py

--- 0x09-python-everything_is_object/101-locked_class.py
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -5,10 +5,4 @@
 class LockedClass:
     """defines a class that prevents the user from dynamically creating
     attributes, except if it is first_name"""
-
-    def __setattr__(self, key, value):
-        """Sets a new attribute for the object"""
-        if not hasattr(self, key) and key != "first_name":
-            raise AttributeError("'LockedClass' object has"
-                                 f" no attribute '{key}'")
-        object.__setattr__(self, key, value)
+    __slots__ = ["first_name"]
