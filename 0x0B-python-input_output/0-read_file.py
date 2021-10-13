c3c2a420374306a7ada48e03a5b6eb3b796adc2e
OVECJOE <ovecjoe123@gmail.com>
2021-10-13 16:03:55 +0100

    Added 0-read_file.py

---
+++ 0x0B-python-input_output/0-read_file.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""Contains read_file(...) function"""
+
+
+def read_file(filename=""):
+    """reads a text file (UTF-8) and print it to stdout"""
+    with open(filename, encoding="utf-8") as f:
+        f.read()
