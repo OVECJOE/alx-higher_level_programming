81ac2eed4968269fa27099abbce09f397b32f8aa
OVECJOE <ovecjoe123@gmail.com>
2021-10-19 08:49:06 +0000

    Updated test_rectangle.py for task 4

--- 0x0C-python-almost_a_circle/tests/test_models/test_rectangle.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_rectangle.py
@@ -7,8 +7,8 @@ from models.rectangle import Rectangle
 from models.base import Base
 
 
-class TestRectangle(unittest.TestCase):
-    """Test suite for Rectangle class in models.rectangle"""
+class TestRectangleInstantiationAndAttributes(unittest.TestCase):
+    """Test suite for Rectangle instantiation and attributes in models.rectangle"""
 
     def test_rectangle_is_subclass_of_base(self):
         self.assertTrue(issubclass(Rectangle, Base))
@@ -67,3 +67,15 @@ class TestRectangle(unittest.TestCase):
     def test_y_not_under_0(self):
         with self.assertRaises(ValueError):
             rect = Rectangle(2, 1, y=-1)
+
+
+class TestRectangleMethods(unittest.TestCase):
+    """A test suite for the Rectangle methods"""
+
+    def test_area(self):
+        r1 = Rectangle(3, 2)
+        self.assertEqual(r1.area(), 6)
+        r2 = Rectangle(2, 10)
+        self.assertEqual(r2.area(), 20)
+        r3 = Rectangle(8, 7, 0, 0, 12)
+        self.assertEqual(r3.area(), 56)
