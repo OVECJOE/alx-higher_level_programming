6d92312a56cb2b359935d7942c9af58e579d23fa
OVECJOE <ovecjoe123@gmail.com>
2021-11-17 12:05:40 +0100

    Fixed bug in 9-full_creation.sql

--- 0x0D-SQL_introduction/9-full_creation.sql
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -1,6 +1,7 @@
 -- creates a table second_table in the database
 CREATE TABLE IF NOT EXISTS `second_table` (
-	`id` INT, `name` VARCHAR(256), `score` INT);
+	`id` INT NOT NULL PRIMARY KEY AUTO_INCREMENT, 
+	`name` VARCHAR(256), `score` INT);
 INSERT INTO `second_table` (`id`, `name`, `score`) VALUES (NULL, "John", 10);
 INSERT INTO `second_table` (`id`, `name`, `score`) VALUES (NULL, "Alex", 3);
 INSERT INTO `second_table` (`id`, `name`, `score`) VALUES (NULL, "Bob", 14);
