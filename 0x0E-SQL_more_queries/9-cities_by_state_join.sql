31dbdcc6c4ca3b38c71032e24a4e27ffddf7b6c0
OVECJOE <ovecjoe123@gmail.com>
2021-11-18 11:43:55 +0100

    Added 9-cities_state_join.sql

--- 0x0E-SQL_more_queries/9-cities_by_state_join.sql
+++ 0x0E-SQL_more_queries/9-cities_by_state_join.sql
@@ -1,4 +1,5 @@
 -- lists all cities contained in the database hbtn_0d_usa
 SELECT id, `cities`.`name`, `cities`.`name`
-FROM `cities`, `states`
+FROM `cities`
+INNER JOIN `states`
 ORDER BY `cities`.`id`;
