627ce0e58634253df14de1d0e0871cbb3b021722
OVECJOE <ovecjoe123@gmail.com>
2021-11-18 10:33:02 +0100

    Added 6-states.sql

--- 0x0E-SQL_more_queries/6-states.sql
+++ 0x0E-SQL_more_queries/6-states.sql
@@ -1,5 +1,5 @@
 -- creates the database hbtn_0d_usa and the table states
-CREATE USER IF NOT EXISTS 'hbtn_0d_usa'@'localhost';
-CREATE TABLE IF NOT EXISTS states (
-	id INT UNIQUE AUTO_INCREMENT NOT NULL PRIMARY KEY,
+CREATE USER IF NOT EXISTS hbtn_0d_usa;
+CREATE TABLE IF NOT EXISTS hbtn_0d_02.states(
+	id INT NOT NULL AUTO_INCREMENT,
 	name VARCHAR(256) NOT NULL);
