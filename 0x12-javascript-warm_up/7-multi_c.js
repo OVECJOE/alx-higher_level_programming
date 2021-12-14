2750ece1d861b1124baaf01f17d16c00486102ba
OVECJOE <ovecjoe123@gmail.com>
2021-12-14 19:00:04 +0100

    Task 8 done :tada:

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -2,10 +2,9 @@
 let x = require('process').argv[2];
 x = parseInt(x);
 if (isNaN(x)) {
-	console.log('Missing number of occurrences');
-}
-else {
-	for (let i = 0; i < x; i++) {
-		console.log('C is fun');
-	}
+  console.log('Missing number of occurrences');
+} else {
+  for (let i = 0; i < x; i++) {
+    console.log('C is fun');
+  }
 }
