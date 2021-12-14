4adc67da23a7a20543d2c87e1f292c163c42a0b4
OVECJOE <ovecjoe123@gmail.com>
2021-12-14 18:15:06 +0100

    Fixed style complaints

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -1,9 +1,8 @@
 #!/usr/bin/node
 const args = require('process').argv;
-let myVar = parseInt(args[2]);
+const myVar = parseInt(args[2]);
 if (!isNaN(myVar)) {
-	console.log(myVar);
-}
-else {
-	console.log('Not a number');
+  console.log(myVar);
+} else {
+  console.log('Not a number');
 }
