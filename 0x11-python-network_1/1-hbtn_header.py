bccae91bfa4cd84af2ddcdc02be07b78011257ca
OVECJOE <ovecjoe123@gmail.com>
2022-01-14 19:23:41 +0100

    Task 1: fixed default return value for get method

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -7,4 +7,4 @@ import sys
 if __name__ == "__main__":
     url = sys.argv[1]
     with urlopen(url) as response:
-        print(response.headers.get('X-Request-Id', ''))
+        print(response.headers.get('X-Request-Id', None))
